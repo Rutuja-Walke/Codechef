7f0a5915339dbe87f5610dfeaaf4f9ac7e4dc248
Rutuja Walke <68474231+Rutuja-Walke@users.noreply.github.com>
2021-04-12 02:21:56 +0530

    Update save_konoha.cpp

--- Codechef DSA Learning Series/Standard Template Library/save_konoha.cpp
+++ Codechef DSA Learning Series/Standard Template Library/save_konoha.cpp
@@ -9,8 +9,8 @@ using namespace std;
 #define ll long long
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);   
+    /*ios_base::sync_with_stdio(false);
+    cin.tie(NULL);   */
 	int t;
 	cin>>t;
 	while(t--){
